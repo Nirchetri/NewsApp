c1c4688a39a7936f677be04b86b55e60b1998f07
unknown <nikhilchetry942@gmail.com>
2024-02-02 01:19:19 +0530

    reactapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState} from 'react';
 import './App.css';
+import News from './News';
+
+
 
 function App() {
+
+
+  let [articles,setArticles] = useState([]);
+  let [category,setCategory] = useState("global");
+
+  useEffect(()=>{
+
+ 
+   fetch('https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=e0d4625cd6e948b9854eab04a9c3cbaf')
+    .then((response)=>response.json())
+    .then((news)=>{
+     setArticles(news.articles);
+     console.log(news.articles);
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+
+  },[category])
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <header className='header'>
+      <h1>Global News</h1>
+      <input type='text' onChange={(event)=>{
+        if(event.target.value!=="")
+        {
+          setCategory(event.target.value);
+        }
+        else
+        {
+          setCategory("global")
+        }
+       
+
+      }} placeholder='Search News'/>
+
+    </header>
+
+
+     <section className="news-articles">
+    {
+       articles.map((article)=>{
+        return(
+          <News article ={article}/>
+        )
+       })
+    }
+     
+     </section> 
+
+      </div>
   );
 }
 
